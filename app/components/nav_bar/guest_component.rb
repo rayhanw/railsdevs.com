c8131dd53ea82d5640ead0573d63314150547206
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-07-29 14:16:11 +0800

    refactor: use class_names on GuestComponent

--- app/components/nav_bar/guest_component.rb
+++ app/components/nav_bar/guest_component.rb
@@ -5,5 +5,12 @@ module NavBar
     def initialize(links:)
       @links = links
     end
+
+    def nav_classes
+      [
+        "bg-gray-800",
+        "fixed top-0 left-0 right-0 z-50": helpers.turbo_native_app?
+      ]
+    end
   end
 end
