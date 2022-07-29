49e61f3a0d5640fc3d2f5a4f8f6f8865e59f7046
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-07-29 14:15:59 +0800

    refactor: use class_names on CardComponent

--- app/components/developers/card_component.rb
+++ app/components/developers/card_component.rb
@@ -17,6 +17,17 @@ module Developers
       @developer.bio
     end
 
+    def container_classes
+      ["relative", "border-b": featured?]
+    end
+
+    def link_classes
+      # #{"border-l-4 border-blue-400" if featured?}
+      base_classes = "bg-white hover:bg-gray-50 px-4 py-6 sm:p-6 flex space-x-4 sm:space-x-8".split
+
+      [*base_classes, "border-l-4 border-blue-400": featured?]
+    end
+
     def featured?
       !!@featured
     end
